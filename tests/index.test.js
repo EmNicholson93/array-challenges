b54cf07c25003ae5580b798f68702b9534027fce
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-07-08 16:49:31 -0700

    minor refactor

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,10 +1,20 @@
 const { multiplesOfN } = require('../lib/index');
 
 describe('mupltiples of n', () => {
-  it('returns an array of nums that are mutiples of a num', () => {
-    const num = 5;
-    const multiples = multiplesOfN(num);
+  it('returns an array of nums that are mutiples of 5', () => {
+  
+    const multiples = multiplesOfN(5);
     
     expect(multiples).toEqual([5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]);
+  });
+  it('returns an array of nums that are mutiples of 15', () => {
+    const multiples = multiplesOfN(15);
+    
+    expect(multiples).toEqual([15, 30, 45, 60, 75, 90]);
+  });
+  it('returns no array of nums that are mutiples of 113', () => {
+    const multiples = multiplesOfN(113);
+    
+    expect(multiples).toEqual([]);
   });
 });
